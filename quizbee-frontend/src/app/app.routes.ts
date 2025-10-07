597985de6004666b08f200b7fefaa55c8862f4f8
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-07 02:57:38 -0500

    feat(routing): integrate ranking routes into main application

--- quizbee-frontend/src/app/app.routes.ts
+++ quizbee-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {Layout} from './shared/presentation/components/layout/layout';
 import {Home} from './shared/presentation/views/home/home';
+import { rankingRoutes } from './ranking/presentation/views/ranking.routes';
+
 const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
 const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
 const baseTitle = 'Quizbee';
@@ -9,4 +11,5 @@ export const routes: Routes = [
   { path: 'about', loadComponent: about, title: `${baseTitle} - About`  },
   { path: '', redirectTo: '/home', pathMatch: 'full'  },
   { path: '**', loadComponent:  pageNotFound, title: `${baseTitle} - Page Not Found`  },
+  { path: 'ranking', loadChildren: () => import('./ranking/presentation/views/ranking.routes').then(m => m.rankingRoutes) },
   ];
