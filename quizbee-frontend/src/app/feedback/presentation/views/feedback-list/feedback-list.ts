1cb327a20f7d20df980cb2175b9dbf03ae80f6c0
DiegoLlamozas <diegollamozas911@gmail.com>
2025-10-26 07:00:55 -0500

    feat: implement feedback list component

---
+++ quizbee-frontend/src/app/feedback/presentation/views/feedback-list/feedback-list.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatError } from '@angular/material/form-field';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { FeedbackStore } from '../../../application/feedback-store';
+import { Feedback } from '../../../domain/model/feedback.entity';
+
+@Component({
+  selector: 'app-feedback-list',
+  standalone: true,
+  imports: [
+    MatTableModule,
+    MatButtonModule,
+    MatError,
+    MatProgressSpinner
+  ],
+  templateUrl: './feedback-list.component.html',
+  styleUrl: './feedback-list.component.css'
+})
+export class FeedbackList {
+  readonly store = inject(FeedbackStore);
+  protected router = inject(Router);
+
+  displayedColumns: string[] = ['id', 'quizId', 'rating', 'comment', 'actions'];
+
+  editFeedback(id: number) {
+    this.router.navigate(['learning/feedback/edit', id]).then();
+  }
+
+  deleteFeedback(id: number) {
+    this.store.deleteFeedback(id);
+  }
+
+  viewResponses(feedbackId: number) {
+    this.router.navigate(['learning/feedback', feedbackId, 'responses']).then();
+  }
+}
